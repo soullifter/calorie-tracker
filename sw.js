528405c86bdb2446c52da3d0ed3cc294264b8c7b
yashwanth.adunukota <yashwanth.adunukota@servicenow.com>
2026-08-31 15:35:08 -0700

    Updates

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
-const CACHE_NAME = 'calorie-tracker-v1'
+const CACHE_NAME = 'calorie-tracker-v2'
 
-self.addEventListener('install', (e) => {
-  self.skipWaiting()
-})
+self.addEventListener('install', () => self.skipWaiting())
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
@@ -14,7 +12,6 @@ self.addEventListener('activate', (e) => {
 })
 
 self.addEventListener('fetch', (e) => {
-  // Skip non-GET and API requests
   if (e.request.method !== 'GET') return
   const url = new URL(e.request.url)
   if (url.origin !== location.origin) return
@@ -22,8 +19,11 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
       .then((res) => {
-        const clone = res.clone()
-        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone))
+        // Only cache successful responses
+        if (res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone))
+        }
         return res
       })
       .catch(() => caches.match(e.request))
